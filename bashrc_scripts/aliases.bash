0b3d9bf7c199ccde5946ae657b6e70a244d224d9
Markus Hoofe <markus.hoofe@bayard-consulting.com>
2016-06-13 18:46:31 +0200

    Updated aliases

--- bashrc_scripts/aliases.bash
+++ bashrc_scripts/aliases.bash
@@ -1,28 +1,34 @@
 
-# Set customized aliases
+# Use diff with colors, if available
+[[ -n "$(type -t colordiff)" ]] && alias diff='colordiff'
+
+# Use grep with colors
+alias grep='grep --color=auto'
+
+# Use ls with colors, if available
 if [[ "$OSTYPE" == "darwin"* ]] && [[ "$(type -p ls)" == "/bin/ls" ]]; then
     alias ls='ls -CF'
 else
     alias ls='ls -CF --color=auto --time-style=locale'
 fi
+
 alias ll='ls -l'
 alias la='ls -la'
 alias dir='ls -A'
+#alias l.='ls -d .[[:alpha:]]*'
 
 alias cd..='cd ..'
 alias ..='cd ..'
+alias up='cd ..'
 
 alias cp='cp -i'
 alias mv='mv -i'
+#alias rm='rm -i'
 
+alias cls=clear
 alias h='history'
 alias md='mkdir'
 alias rd='rmdir'
-
-alias grep='grep --color=auto'
-
-# Use colordiff, if available
-[[ -n "`type -t colordiff`" ]] && alias diff='colordiff'
 
 alias r+='chmod a+r'
 alias w+='chmod ug+w'
@@ -30,4 +36,7 @@ alias x+='chmod a+x'
 alias r-='chmod go-r'
 alias w-='chmod a-w'
 alias x-='chmod a-x'
+
+alias df='df -h'
+alias du='du -h'
 
