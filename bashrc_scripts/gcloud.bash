a34ea7472e32251528d30fa835aa08070d624313
Markus Hoofe <markus.hoofe@bayard-consulting.com>
2016-06-17 14:34:43 +0200

    Correct gcloud search paths

--- bashrc_scripts/gcloud.bash
+++ bashrc_scripts/gcloud.bash
@@ -1,10 +1,6 @@
 
-unset prefix
-[[ -n "$(type -p brew)" ]] && prefix="$(brew --prefix)/opt"
-[[ -z "$prefix" ]] && prefix="/usr/local"
-
 # Set Google Cloud SDK Home
-if cloudsdk_home="$(findFirstDir "$HOME/opt/google-cloud-sdk" "${prefix}/share/google/google-cloud-sdk")"; then
+if cloudsdk_home="$(findFirstDir "$HOME/opt/google-cloud-sdk" "/usr/local/share/google/google-cloud-sdk" "/usr/lib/google-cloud-sdk")"; then
     export CLOUDSDK_HOME="$cloudsdk_home"
 fi
 unset cloudsdk_home
