ca61a7bb8338d74ce44ed6e85eb0936ae67a1376
Markus Hoofe <markus.hoofe@bayard-consulting.com>
2020-06-15 17:37:51 +0200

    Updated gcloud home directory locations

--- bashrc_scripts/gcloud.bash
+++ bashrc_scripts/gcloud.bash
@@ -1,7 +1,7 @@
 
 # Set Google Cloud SDK Home
-if cloudsdk_home="$(findFirstDir "$HOME/opt/google-cloud-sdk" "/usr/local/share/google/google-cloud-sdk" "/usr/lib/google-cloud-sdk")"; then
-    export CLOUDSDK_HOME="$cloudsdk_home"
+if cloudsdk_home="$(findFirstDir "$HOME/opt/google-cloud-sdk" "/usr/local/share/google/google-cloud-sdk" "/usr/lib/google-cloud-sdk" "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk")"; then
+  export CLOUDSDK_HOME="$cloudsdk_home"
 fi
 unset cloudsdk_home
 
@@ -16,6 +16,6 @@ sourceScript "${CLOUDSDK_HOME}/completion.bash.inc"
 
 # Add shell completion to kubectl, if available
 if [[ "$(type -p kubectl)" == "${CLOUDSDK_HOME}/bin/kubectl" ]]; then
-    source <(kubectl completion bash)
+  source <(kubectl completion bash)
 fi
 
