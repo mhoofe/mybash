1d88b7a7e01f43a93d92e5f024c8e56345cc81b9
Markus Hoofe <markus.hoofe@bayard-consulting.com>
2022-02-08 23:09:07 +0100

    Added iterm2 scripts to PATH again

--- bashrc_scripts/iterm.bash
+++ bashrc_scripts/iterm.bash
@@ -1,3 +1,8 @@
 
-sourceScript "$HOME/.iterm2_shell_integration.bash"
+# sourceScript "$HOME/.iterm2_shell_integration.bash"
+
+if iterm2_home="$(findFirstDir "$HOME/.iterm2")"; then
+  appendPath PATH "$iterm2_home"
+fi
+unset iterm2_home
 
