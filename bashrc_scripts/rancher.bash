a8ad6c8616e03e4f6b306e6e27e50748fb6a5d6e
Markus Hoofe <markus.hoofe@bayard-consulting.com>
2023-03-20 20:16:25 +0100

    Corrected rancher script, added DOCKER_HOST

--- bashrc_scripts/rancher.bash
+++ bashrc_scripts/rancher.bash
@@ -1,4 +1,12 @@
 
+# Set nvm home directory
+RANCHER_DIR="${HOME}/.rd"
+[[ ! -d "$RANCHER_DIR" ]] && return
+export RANCHER_DIR
+
 # Add rancher bin to path
-appendPath PATH "$HOME/.rd/bin"
+appendPath PATH "${RANCHER_DIR}/bin"
+
+# Set DOCKER_HOST socket
+export DOCKER_HOST="unix://${RANCHER_DIR}/docker.sock"
 
